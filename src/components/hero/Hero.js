cb2b731eadb0491cc66b9e0550b04dce8882966f
Anthony64M <jymal6anthony4@gmail.com>
2023-03-27 00:55:44 +0300

    edit navbar

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
@@ -6,6 +6,8 @@ import MainVideo from '../../assets/video.mp4';
 import './Hero.css';
 
 const Hero = () => {
+
+  const [language, setLanguage] = useState('english');
 
   const handleJoinUs = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -19,18 +21,32 @@ const Hero = () => {
       });
   };
 
+  const switchLanguage = () => {
+    setLanguage(language === 'english' ? 'german' : 'english');
+  };
+
   return (
     <div className='hero'>
       <video autoPlay loop muted id='video'>
         <source src={MainVideo} type='video/mp4'/>
       </video>
       <div className="hero-text">
-        <h1>Run wild, run free</h1>
-        <h1>No frills,<span className="blue">just thrills</span> </h1>
-        <p>Connect with <span className="blue">your body and the environment</span> on our beautiful paths.</p>
+        {language === 'english' ? (
+          <>
+            <h1>Run wild, run free</h1>
+            <h1>No frills,<span className="blue">just thrills</span> </h1>
+            <p>Connect with <span className="blue">your body and the environment</span> on our beautiful paths.</p>
+          </>
+        ) : (
+          <>
+            <h1>Lauf wild, lauf frei</h1>
+            <h1>Kein Schnickschnack,<span className="blue">nur Nervenkitzel</span> </h1>
+            <p>Verbinde dich mit <span className="blue">deinem Körper und der Umwelt</span> auf unseren wunderschönen Pfaden.</p>
+          </>
+        )}
         <div className="btn-group">
-          <button className="btn" onClick={handleJoinUs}>Join us</button>
-          <button className="btn btn-outline">FAQS</button>
+          <button className="btn" onClick={handleJoinUs}>{language === 'english' ? 'Join us' : 'Mach mit'}</button>
+          <button className="btn btn-outline" onClick={switchLanguage}>{language === 'english' ? 'Switch to German' : 'Zurück zu Englisch'}</button>
         </div>
       </div>
     </div>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,33 +1,53 @@
-import React, {useState} from 'react'
-import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
-import './Navbar.css'
+import React, { useState, useEffect } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import './Navbar.css';
+import Terminal from '../../assets/term.png';
 
 const Navbar = () => {
-    const [click, setClick] = useState(false)
-    const handleClick = () => setClick(!click)
+  const [click, setClick] = useState(false);
+  const [navbarTransparent, setNavbarTransparent] = useState(false);
 
-    return (
-        <div className='navbar'>
-            <div className="container">
-                <h1 style={{ marginLeft: '1rem', color: '#00d8ff'}} >BednTrail</h1>
-                <ul className={click ? 'nav active' : 'nav'}>
-                    <li className="nav-item">
-                        <a href="/">Walk</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="/">Bike</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="/">Run</a>
-                    </li>
-                </ul>
-                <div onClick={handleClick} className="hamburger">
-                    {click ? (<AiOutlineClose className='icon' />) : (<AiOutlineMenu className='icon' />)}
-                    
-                </div>
-            </div>
+  const handleClick = () => setClick(!click);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setNavbarTransparent(true);
+      } else {
+        setNavbarTransparent(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div className={`navbar ${navbarTransparent ? 'transparent' : ''}`}>
+      <div className="container">
+        <div className="img-container">
+          <img src={Terminal} alt="" />
         </div>
-    )
-}
+        <ul className={click ? 'nav active' : 'nav'}>
+          <li className="nav-item">
+            <a href="/">Walk</a>
+          </li>
+          <li className="nav-item">
+            <a href="/">Bike</a>
+          </li>
+          <li className="nav-item">
+            <a href="/">Run</a>
+          </li>
+        </ul>
+        <div onClick={handleClick} className="hamburger">
+          {click ? (
+            <AiOutlineClose className="icon" />
+          ) : (
+            <AiOutlineMenu className="icon" />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
