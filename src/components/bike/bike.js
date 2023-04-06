ad9963c0e64b089227d9d91805fa0805d5b4a167
Anthony64M <jymal6anthony4@gmail.com>
2023-04-06 16:18:21 +0300

    navbar to black

--- src/components/bike/bike.js
+++ src/components/bike/bike.js
@@ -116,77 +116,76 @@ if (accessToken) {
   Strava.athlete.listActivities({
     access_token: accessToken,
     per_page: 10,
-    if (err) {
-      this.setState({ error: `Error retrieving athlete: ${err.message}` });
-    } else {
-      this.setState({ athlete: payload });
-    }
-
-class Bike extends React.Component {
-  constructor(props) {
+  }, (err, payload) => {
+    if (err) {error: "Access token not found." });
+  }
+  class Bike extends React.Component {
+    constructor(props) {
     super(props);
     this.state = {
-      accessToken: null,
-      activities: null,
-      athlete: null,
-      error: null,
+    accessToken: null,
+    activities: null,
+    error: null,
     };
-  }
-
-  componentDidMount() {
+    }
+    
+    componentDidMount() {
     const accessToken = localStorage.getItem("access_token");
     this.setState({ accessToken }, () => {
-      if (accessToken) {
-        Strava.athlete.get({ access_token: accessToken }, (err, athletePayload) => {
-          if (err) {
-            this.setState({ error: `Error retrieving athlete: ${err.message}` });
-          } else {
-            this.setState({ athlete: athletePayload });
-          }
-        });
-
-        Strava.athlete.listActivities(
-          {
-            access_token: accessToken,
-            per_page: 10,
-          },
-          (err, activitiesPayload) => {
-            if (err) {
-              this.setState({
-                error: `Error retrieving activities: ${err.message}`,
-              });
-            } else {
-              this.setState({ activities: activitiesPayload });
-            }
-          }
-        );
-      } else {
-        this.setState({ error: "Access token not found." });
-      }
+    if (accessToken) {
+    Strava.athlete.get({ access_token: accessToken }, (err, payload) => {
+    if (err) {
+    this.setState({ error: Error retrieving athlete: ${err.message} });
+    } else {
+    this.setState({ athlete: payload });
+    }
     });
+    } else {
+    this.setState({ error: "Access token not found." });
+    }
+    });
+    if (accessToken) {
+      Strava.athlete.listActivities(
+        {
+          access_token: accessToken,
+          per_page: 10,
+        },
+        (err, payload) => {
+          if (err) {
+            this.setState({
+              error: `Error retrieving activities: ${err.message}`,
+            });
+          } else {
+            this.setState({ activities: payload });
+          }
+        }
+      );
+    }
   }
 
   render() {
-    const { accessToken, athlete, activities, error } = this.state;
-    if (!accessToken) {
-      return <div>{error}</div>;
-    }
-    return (
-      <div>
-        <h1>{athlete.firstname}'s Last 10 Rides</h1>
-        {error && <div>{error}</div>}
-        {activities && (
-          <ul>
-            {activities.map((activity) => (
-              <li key={activity.id}>
-                {activity.name} - {activity.distance}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    );
+  const { accessToken, athlete, activities, error } = this.state;
+  if (!accessToken) {
+  return <div>{error}</div>;
   }
-}
+  return (
+  <div>
+  <h1>{athlete.firstname}'s Last 10 Rides</h1>
+  {error && <div>{error}</div>}
+  {activities && (
+  <ul>
+  {activities.map((activity) => (
+  <li key={activity.id}>
+  {activity.name} - {activity.distance}
+  </li>
+  ))}
+  </ul>
+  )}
+  </div>
+  );
+  }
+  }
+  
+  export default Bike;
+  
 
-export default Bike;
